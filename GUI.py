e4e2af38f8252fb4fd00e90a4d774d038785c05f
Justi29 <61147766+Justi29@users.noreply.github.com>
2020-12-04 11:39:34 +0100

    Update and rename GUI_2.py to GUI.py

--- GUI.py
+++ GUI.py
@@ -11,6 +11,8 @@ from tkinter import *
 from tkinter import messagebox as msb
 from tempfile import NamedTemporaryFile
 import shutil
+from tkinter.ttk import *
+import os 
 
 root = Tk()
 product = StringVar()
@@ -19,7 +21,7 @@ amount = StringVar()
 #-----------------------------------------------------------------------------------------------
       
 def show():
-    global amountlabel
+    global Productlabel, amountlabel, index
     if listbox1.curselection():
         index = listbox1.curselection()[0]
     else:
@@ -27,15 +29,16 @@ def show():
         
     if(index==-1):
         print("error")
-        msb.showerror(title="Error", message="please select a product from the list")
+        msb.showerror(title="Error", message="Please select a product from the list")
     else:
-        Productlabel = Label(root, text="Quantity in the store").grid(row=7, column=0)
+        Productlabel = Label(root, text="Quantity in the store")
+        Productlabel.grid(row=7, column=0)
         amountlabel = Label(root, text = Data[index][2])
         amountlabel.grid(row=7, column=1)
     
     
 def showProducer():
-    global amountlabel1
+    global Productlabel1, amountlabel1, index
     if listbox1.curselection():
         index = listbox1.curselection()[0]
     else:
@@ -43,9 +46,9 @@ def showProducer():
         
     if(index==-1):
         print("error")
-        msb.showerror(title="Error", message="please select a product from the list")
+        msb.showerror(title="Error", message="Please select a product from the list")
     else:
-        File1 = open('/Users/justi/Desktop/IoT/Assignment_2/Inventory-Management/Producer.csv')
+        File1 = open('Producer.csv')
         Reader1 = csv.reader(File1)
         Data1 = list(Reader1)
         del(Data1[0])
@@ -54,7 +57,8 @@ def showProducer():
         for x in list(range(0,len(Data1))):
             list_of_products1.append(Data1[x][1])
         #if(Data[index][1] in  Data1):
-        Productlabel1 = Label(root, text="Quantity in the producer").grid(row=8, column=0)
+        Productlabel1 = Label(root, text="Quantity in the producer")
+        Productlabel1.grid(row=8, column=0)
         amountlabel1 = Label(root, text = Data1[index][2])
         amountlabel1.grid(row=8, column=1)
         
@@ -62,7 +66,7 @@ def showProducer():
         
         
 def placeOrder():
-    global OrderEntry
+    global Orderlabel, OrderEntry, index, btn2, button3
     if listbox1.curselection():
         index = listbox1.curselection()[0]
     else:
@@ -70,11 +74,13 @@ def placeOrder():
         
     if(index==-1):
         print("error")
-        msb.showerror(title="Error", message="please select a product from the list")
+        msb.showerror(title="Error", message="Please select a product from the list")
     else:
-        Orderlabel = Label(root, text="Quantity to order").grid(row=10, column=0)
+        Orderlabel = Label(root, text="Quantity to order")
+        Orderlabel.grid(row=10, column=0)
     
-        OrderEntry = Entry(root, textvariable = amount, width = 10).grid(row=10, column=1)
+        OrderEntry = Entry(root, textvariable = amount, width = 10)
+        OrderEntry.grid(row=10, column=1)
     
         btn2 = Button(root, text="Confirm", command=order)
         btn2.grid(row=10, column=2)
@@ -88,29 +94,36 @@ def placeOrder():
      
 
 def order():
-    
+    global prod_loc
     TheAmount = amount.get()
     if(TheAmount==""):
         print("error")
         msb.showerror(title="error", message="Please provide the amount of product to order")
     else:
-        filename = '/Users/justi/Desktop/IoT/Assignment_2/Inventory-Management/Store.csv'
-        tempfile = NamedTemporaryFile(delete=False)
-
-        fields = ['id', 'product_name', 'quantity']
-
-        with open(filename, 'r') as csvfile, tempfile:
-            reader = csv.DictReader(csvfile)
-            writer = csv.DictWriter(tempfile, fieldnames=fields)
-            for row in reader:
-                if row['product_name'] == str(Data[index][1]):
-                    print('updating the amount of ', row['product_name'])
-                    row['quantity'] = TheAmount
-                row = {'id':row['id'],'product_name': row['product_name'], 'quantity': row['quantity']}
-                writer.writerow(dict(row))
-
-        shutil.move(tempfile, filename)
-        msb.showinfo(title="Success", message="Order was placed")
+# =============================================================================
+#         filename = '/Users/justi/Desktop/IoT/Assignment_2/Store.csv'
+#         tempfile = NamedTemporaryFile(delete=False)
+# 
+#         fields = ['id', 'product_name', 'quantity']
+# 
+#         with open(filename, 'r') as csvfile, tempfile:
+#             reader = csv.DictReader(csvfile)
+#             writer = csv.DictWriter(tempfile, fieldnames=fields)
+#             for row in reader:
+#                 if row['product_name'] == str(Data[index][1]):
+#                     print('updating the amount of ', row['product_name'])
+#                     row['quantity'] = TheAmount
+#                 row = {'id':row['id'],'product_name': row['product_name'], 'quantity': row['quantity']}
+#                 writer.writerow(dict(row))
+#          shutil.move(tempfile, filename)
+# =============================================================================
+        prod_loc = Data[index][3]
+        print(prod_loc)
+        os.system('python __init__.py')
+        
+        msb.showinfo(title="Success", message="Order was placed! \n")
+        msb.showinfo(title="Delivery", message=delivery)
+        
         listbox1.config(state="normal")
         button1.config(state="normal")
         
@@ -119,18 +132,21 @@ def order():
 
 def cancel():
         listbox1.config(state="normal")
-        button1.config(state="normal")  
-        #Orderlabel.config(text="")
-        #OrderEntry.set("")
-        #amountlabel1.text.set("Text updated")
-        #amountlabel.text.set("Text updated")
-        #btn2.pack_forget()
-        #button3.pack_forget()
+        button1.config(state="normal") 
+        Productlabel.destroy()
+        Productlabel1.destroy()
+        Orderlabel.destroy()
+        OrderEntry.destroy()
+        amountlabel1.destroy()
+        amountlabel.destroy()
+        btn2.destroy()
+        button3.destroy()
+        index = None
         
   
 
 #-----------------------------------------------------------------------------------------------
-File = open('/Users/justi/Desktop/IoT/Assignment_2/Inventory-Management/Store.csv')
+File = open('Store.csv')
 Reader = csv.reader(File)
 Data = list(Reader)
 del(Data[0])
